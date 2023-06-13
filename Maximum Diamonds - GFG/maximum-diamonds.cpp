ec9e12656c0e47fb172f0a7ebf785f8ab8078de1
Mohammad Asif <100487784+asif-0007@users.noreply.github.com>
2023-06-14 00:38:25 +0530

    Added solution - LeetHub

---
+++ Maximum Diamonds - GFG/maximum-diamonds.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long maxDiamonds(int A[], int N, int K) {
+        // code here
+        priority_queue<int>pq;
+        long long ans = 0;
+        for(int i=0;i<N;i++){
+            pq.push(A[i]);
+        }
+        for(int i=0;i<K;i++){
+            int t = pq.top();
+            ans+=t;
+            pq.pop();
+            pq.push(t/2);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,K;
+        
+        cin>>N>>K;
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.maxDiamonds(A,N,K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
