72847d1e75bdb9f6c471636a73512aa7a1261dec
Mohammad Asif <100487784+asif-0007@users.noreply.github.com>
2023-06-13 15:21:34 +0530

    Added solution - LeetHub

--- Overlapping Intervals - GFG/overlapping-intervals.cpp
+++ Overlapping Intervals - GFG/overlapping-intervals.cpp
@@ -6,18 +6,32 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
+        // int n = intervals.size();
+        // sort(intervals.begin(),intervals.end());
+        // vector<vector<int>> ans;
+        // for(int i=0;i<n;i++){
+        //     int start = intervals[i][0];
+        //     int end   = intervals[i][1];
+        //     if(!ans.empty() && end<=ans.back()[1])continue;
+        //     for(int j=i+1;j<n;j++){
+        //         if(intervals[j][0]<=end)end=max(end,intervals[j][1]);
+        //         else break;
+        //     }
+        //     ans.push_back({start,end});
+        // }
+        // return ans;
+        
+        //2nd approach in O(N) one pass
         int n = intervals.size();
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
-            int start = intervals[i][0];
-            int end   = intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1])continue;
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end)end=max(end,intervals[j][1]);
-                else break;
+            if(ans.empty() || ans.back()[1] < intervals[i][0]){
+                ans.push_back(intervals[i]);
             }
-            ans.push_back({start,end});
+            else{
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            }
         }
         return ans;
     }
