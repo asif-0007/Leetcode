41f8426e5eb70be7527e38779ce8c442e9f67cc0
Mohammad Asif <100487784+asif-0007@users.noreply.github.com>
2023-06-21 19:27:22 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -10,24 +10,57 @@ class Solution
 {
     public:
     //Function to find total number of unique paths.
-    int countWaysUtil(int i, int j, vector<vector<int> > &dp) {
-      if(i==0 && j == 0)
-        return 1;
-      if(i<0 || j<0)
-        return 0;
-      if(dp[i][j]!=-1) return dp[i][j];
+//     int countWaysUtil(int i, int j, vector<vector<int> > &dp) {
+//       if(i==0 && j == 0)
+//         return 1;
+//       if(i<0 || j<0)
+//         return 0;
+//       if(dp[i][j]!=-1) return dp[i][j];
         
-      int up = countWaysUtil(i-1,j,dp);
-      int left = countWaysUtil(i,j-1,dp);
+//       int up = countWaysUtil(i-1,j,dp);
+//       int left = countWaysUtil(i,j-1,dp);
       
-      return dp[i][j] = up+left;
+//       return dp[i][j] = up+left;
+  
+// }
+//     int NumberOfPath(int m, int n)
+//     {
+//         //code here
+//         vector<vector<int> > dp(m,vector<int>(n,-1));
+//         return countWaysUtil(m-1,n-1,dp);
+//     }
+
+     int countWaysUtil(int m, int n, vector<vector<int> > &dp) {
+          for(int i=0; i<m ;i++){
+              for(int j=0; j<n; j++){
+                  
+                  //base condition
+                  if(i==0 && j==0){
+                      dp[i][j]=1;
+                      continue;
+                  }
+                  
+                  int up=0;
+                  int left = 0;
+                  
+                  if(i>0) 
+                    up = dp[i-1][j];
+                  if(j>0)
+                    left = dp[i][j-1];
+                    
+                  dp[i][j] = up+left;
+              }
+          }
+          
+          return dp[m-1][n-1];
+
   
 }
-    int NumberOfPath(int m, int n)
-    {
-        //code here
+
+    int NumberOfPath(int m, int n){
         vector<vector<int> > dp(m,vector<int>(n,-1));
-        return countWaysUtil(m-1,n-1,dp);
+        return countWaysUtil(m,n,dp);
+        
     }
 };
 
