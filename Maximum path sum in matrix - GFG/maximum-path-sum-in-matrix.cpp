f3a8854390a954e49420311e77f568ad620c5dea
Mohammad Asif <100487784+asif-0007@users.noreply.github.com>
2023-06-23 03:09:38 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -11,38 +11,80 @@ class Solution{
 public:
     int maximumPath(int N, vector<vector<int>> matrix)
     {
-    int n = matrix.size();
+    // int n = matrix.size();
+    // int m = matrix[0].size();
+    
+    // vector<vector<int>> dp(n,vector<int>(m,0));
+    
+    // // Initializing first row - base condition
+    // for(int j=0; j<m; j++){
+    //     dp[0][j] = matrix[0][j];
+    // }
+    
+    // for(int i=1; i<n; i++){
+    //     for(int j=0;j<m;j++){
+            
+    //         int up = matrix[i][j] + dp[i-1][j];
+            
+    //         int leftDiagonal= matrix[i][j];
+    //         if(j-1>=0) leftDiagonal += dp[i-1][j-1];
+    //         else leftDiagonal += -1e9;
+            
+    //         int rightDiagonal = matrix[i][j];
+    //         if(j+1<m) rightDiagonal += dp[i-1][j+1];
+    //         else rightDiagonal += -1e9;
+            
+    //         dp[i][j] = max(up, max(leftDiagonal,rightDiagonal));
+            
+    //     }
+    // }
+    
+    // int maxi = INT_MIN;
+    
+    // for(int j=0; j<m;j++){
+    //     maxi = max(maxi,dp[n-1][j]);
+    // }
+    
+    // return maxi;
+    
+    
+    
+    //space optimized
+    
+     int n = matrix.size();
     int m = matrix[0].size();
     
-    vector<vector<int>> dp(n,vector<int>(m,0));
+    vector<int> prev(m,0), cur(m,0);
     
     // Initializing first row - base condition
     for(int j=0; j<m; j++){
-        dp[0][j] = matrix[0][j];
+        prev[j] = matrix[0][j];
     }
     
     for(int i=1; i<n; i++){
         for(int j=0;j<m;j++){
             
-            int up = matrix[i][j] + dp[i-1][j];
+            int up = matrix[i][j] + prev[j];
             
             int leftDiagonal= matrix[i][j];
-            if(j-1>=0) leftDiagonal += dp[i-1][j-1];
+            if(j-1>=0) leftDiagonal += prev[j-1];
             else leftDiagonal += -1e9;
             
             int rightDiagonal = matrix[i][j];
-            if(j+1<m) rightDiagonal += dp[i-1][j+1];
+            if(j+1<m) rightDiagonal += prev[j+1];
             else rightDiagonal += -1e9;
             
-            dp[i][j] = max(up, max(leftDiagonal,rightDiagonal));
+            cur[j] = max(up, max(leftDiagonal,rightDiagonal));
             
         }
+        
+        prev = cur;
     }
     
     int maxi = INT_MIN;
     
     for(int j=0; j<m;j++){
-        maxi = max(maxi,dp[n-1][j]);
+        maxi = max(maxi,prev[j]);
     }
     
     return maxi;
