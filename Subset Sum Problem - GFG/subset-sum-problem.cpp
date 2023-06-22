71aaf0162ebd3fa71e3a6deaf72d3bc9c9c34c24
Mohammad Asif <100487784+asif-0007@users.noreply.github.com>
2023-06-23 04:32:46 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -33,29 +33,60 @@ public:
         // vector<vector<int>> dp(n,vector<int>(k+1,-1));
         // return subsetSumUtil(n-1,k,arr,dp);
         
-        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
         
-        for(int i=0; i<n; i++){
-            dp[i][0] = true;
-        }
+        // tabulation
+        
+        
+        // vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+        
+        // for(int i=0; i<n; i++){
+        //     dp[i][0] = true;
+        // }
+        
+        // if(arr[0]<=k)
+        //     dp[0][arr[0]] = true;
+        
+        // for(int ind = 1; ind<n; ind++){
+        //     for(int target= 1; target<=k; target++){
+                
+        //         bool notTaken = dp[ind-1][target];
+        
+        //         bool taken = false;
+        //             if(arr[ind]<=target)
+        //                 taken = dp[ind-1][target-arr[ind]];
+            
+        //         dp[ind][target]= notTaken||taken;
+        //     }
+        // }
+        
+        // return dp[n-1][k];
+        
+        
+        // space optimized code 
+        
+        vector<bool> prev(k+1,false);
+    
+        prev[0] = true;
         
         if(arr[0]<=k)
-            dp[0][arr[0]] = true;
+            prev[arr[0]] = true;
         
         for(int ind = 1; ind<n; ind++){
+            vector<bool> cur(k+1,false);
+            cur[0] = true;
             for(int target= 1; target<=k; target++){
-                
-                bool notTaken = dp[ind-1][target];
+                bool notTaken = prev[target];
         
                 bool taken = false;
                     if(arr[ind]<=target)
-                        taken = dp[ind-1][target-arr[ind]];
+                        taken = prev[target-arr[ind]];
             
-                dp[ind][target]= notTaken||taken;
+                cur[target]= notTaken||taken;
             }
+            prev = cur;
         }
         
-        return dp[n-1][k];
+        return prev[k];
     }
 };
 
