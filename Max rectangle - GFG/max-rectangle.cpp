5dad09f9b0d4eb2fd9e1e4312cb7cb253c427e45
Mohammad Asif <100487784+asif-0007@users.noreply.github.com>
2023-07-12 01:55:43 +0530

    Added solution - LeetHub

---
+++ Max rectangle - GFG/max-rectangle.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+
+// } Driver Code Ends
+/*You are required to complete this method*/
+
+class Solution{
+  public:
+    int largestRectangleArea(vector<int>& height) {
+        height.push_back(0);
+        int n = height.size(), area = 0;
+        stack<int> indexes;
+        for (int i = 0; i < n; i++) {
+            while (!indexes.empty() && height[indexes.top()] > height[i]) {
+                int h = height[indexes.top()]; indexes.pop();
+                int l = indexes.empty() ? -1 : indexes.top();
+                area = max(area, h * (i - l - 1));
+            }
+            indexes.push(i);
+        }
+        return area; 
+    }
+    int maxArea(int matrix[MAX][MAX], int n, int m) {
+        int maxArea = 0;
+        vector<int>height(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==1)height[j]++;
+                else height[j]=0;
+            }
+            int area = largestRectangleArea(height);
+            maxArea = max(area,maxArea);
+        }
+        return maxArea;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+
+    int M[MAX][MAX];
+
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> M[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.maxArea(M, n, m) << endl;
+    }
+}
+
+// } Driver Code Ends
