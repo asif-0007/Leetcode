46657af2fb3d1da1d2d614c9b7502fe82098a824
Mohammad Asif <100487784+asif-0007@users.noreply.github.com>
2023-08-03 00:44:09 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -3,61 +3,34 @@
 using namespace std;
 
 // } Driver Code Ends
-
-class Solution{
-// {  //kahn's algorithm for topological sort
-// 	public:
-// 	//Function to return list containing vertices in Topological order. 
-// 	vector<int> topoSort(int V, vector<int> adj[]) 
-// 	{
-// 	    // code here
-// 	    int indegree[V]={0};
-	    
-// 	    for(int i=0;i<V;i++){
-// 	        for(auto it:adj[i]){
-// 	            indegree[it]++;
-// 	        }
-// 	    }
-// 	    queue<int>q;
-// 	    for(int i=0;i<V;i++){
-// 	        if(indegree[i]==0)q.push(i);
-// 	    }
-// 	    vector<int>ans;
-// 	    while(!q.empty()){
-// 	        int node = q.front();
-// 	        q.pop();
-// 	        ans.push_back(node);
-// 	        for(auto it:adj[node]){
-// 	            indegree[it]--;
-// 	            if(indegree[it]==0)q.push(it);
-// 	        }
-// 	    }
-// 	    return ans;
-// 	}
-    public: 
-    vector<int>topoSort(int V, vector<int>adj[]){
-        int indegree[V] = {0};
-        for(int i=0;i<V;i++){
-            for(auto it : adj[i]){
-                indegree[it]++;
-            }
-        }
-        queue<int>q;
-        for(int i = 0;i<V;i++){
-            if(indegree[i]==0)q.push(i);
-        }
-        vector<int>ans;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0)q.push(it);
-            }
-        }
-        return ans;
-    }
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int>topo;
+	    vector<int>indegree(V,0);
+	    for(int i=0;i<V;i++){
+	        for(auto x : adj[i]){
+	            indegree[x]++;
+	        }
+	    }
+	    queue<int>q;
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0)q.push(i);
+	    }
+	    while(!q.empty()){
+	        int node = q.front();q.pop();
+	        topo.push_back(node);
+	        for(auto x:adj[node]){
+	            indegree[x]--;
+	            if(indegree[x]==0)q.push(x);
+	        }
+	    }
+	  return topo;
+	}
 };
 
 //{ Driver Code Starts.
